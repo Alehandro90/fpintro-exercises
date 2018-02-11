8ed604a4bb8a4a307a5ff1d45864bcd37955255a
Aleksandar Ivanov <aleksandar.ivanov@novarto.com>
2018-02-11 22:27:49 +0200

    chapter 2 tasks solved

--- src/main/java/org/example/chapter2/ConsList.java
+++ src/main/java/org/example/chapter2/ConsList.java
@@ -1,19 +1,32 @@
 package org.example.chapter2;
 
+import java.io.File;
+import java.util.function.Function;
+
 import static org.example.Util.error;
 
-public abstract class ConsList<A>
-{
-
-    public static <A> ConsList<A> empty()
-    {
+public abstract class ConsList<A> {
+    public static <A> ConsList<A> empty() {
         return new Nil<>();
     }
 
-    public static <A> ConsList<A> cons(A head, ConsList<A> tail)
-    {
+    public static <A> ConsList<A> cons(A head, ConsList<A> tail) {
         return new Cons<>(head, tail);
     }
+
+    public abstract int length();
+
+    public abstract ConsList<A> append(A element);
+
+    public abstract ConsList<A> reverse();
+
+    public abstract ConsList<A> takeWhile(Function<A, Boolean> condition);
+
+    public abstract ConsList<A> dropWhile(Function<A, Boolean> condition);
+
+    public abstract ConsList<A> filter(Function<A, Boolean> condition);
+
+    public abstract ConsList<A> map(Function<A, A> condition);
 
     public abstract boolean isEmpty();
 
@@ -21,56 +34,162 @@ public abstract class ConsList<A>
 
     public abstract ConsList<A> tail();
 
-    private static final class Cons<A> extends ConsList<A>
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o)
     {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ConsList<A> thisIter = this;
+        ConsList<A> otherIter = (ConsList<A>) o;
+
+        if (this.length() != otherIter.length()) return false;
+
+        while (!thisIter.isEmpty())
+        {
+            if (thisIter.head() == otherIter.head())
+            {
+                thisIter = thisIter.tail();
+                otherIter = otherIter.tail();
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static final class Cons<A> extends ConsList<A> {
         public final A head;
         public final ConsList<A> tail;
 
-        public Cons(A head, ConsList<A> tail)
-        {
+        public Cons(A head, ConsList<A> tail) {
             this.head = head;
             this.tail = tail;
         }
 
-        @Override public boolean isEmpty()
-        {
+        @Override
+        public int length() {
+
+            return 1 + tail.length();
+        }
+
+        @Override
+        public ConsList<A> append(A element) {
+
+            return cons(head, tail.append(element));
+        }
+
+        @Override
+        public ConsList<A> reverse() {
+
+            return tail.isEmpty() ? this : tail.reverse().append(head);
+        }
+
+        @Override
+        public ConsList<A> takeWhile(Function<A, Boolean> condition) {
+
+            return !condition.apply(head) ? empty() : cons(head, tail.takeWhile(condition));
+        }
+
+        @Override
+        public ConsList<A> dropWhile(Function<A, Boolean> condition) {
+
+            return condition.apply(head) ? tail.dropWhile(condition) : this;
+        }
+
+        @Override
+        public ConsList<A> filter(Function<A, Boolean> condition) {
+
+            return condition.apply(head) ? cons(head, tail.filter(condition)) : tail.filter(condition);
+        }
+
+        @Override
+        public ConsList<A> map(Function<A, A> function) {
+
+            return cons(function.apply(head), tail.map(function));
+        }
+
+        @Override
+        public boolean isEmpty() {
             return false;
         }
 
-        @Override public A head()
-        {
+        @Override
+        public A head() {
             return head;
         }
 
-        @Override public ConsList<A> tail()
-        {
+        @Override
+        public ConsList<A> tail() {
             return tail;
         }
     }
 
+    private static final class Nil<A> extends ConsList<A> {
 
-    private static final class Nil<A> extends ConsList<A>
-    {
-
-        public Nil()
-        {
+        public Nil() {
         }
 
-        @Override public boolean isEmpty()
-        {
+        @Override
+        public int length() {
+            return 0;
+        }
+
+        @Override
+        public ConsList<A> append(A element) {
+            return cons(element, empty());
+        }
+
+        @Override
+        public ConsList<A> reverse() {
+            return empty();
+        }
+
+        @Override
+        public ConsList<A> takeWhile(Function<A, Boolean> condition) {
+            return empty();
+        }
+
+        @Override
+        public ConsList<A> dropWhile(Function<A, Boolean> condition) {
+            return empty();
+        }
+
+        @Override
+        public ConsList<A> filter(Function<A, Boolean> condition) {
+            return empty();
+        }
+
+        @Override
+        public ConsList<A> map(Function<A, A> function) {
+            return empty();
+        }
+
+        @Override
+        public boolean isEmpty() {
             return true;
         }
 
-        @Override public A head()
-        {
+        @Override
+        public A head() {
             throw error("head() on Nil");
         }
 
-        @Override public ConsList<A> tail()
-        {
+        @Override
+        public ConsList<A> tail() {
             throw error("tail() on Nil");
         }
     }
+
+
 }
 
 
---
+++ src/main/java/org/example/chapter2/tests/ConslistTest.java
@@ -0,0 +1,163 @@
+package org.example.chapter2.tests;
+
+import org.example.chapter2.ConsList;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.function.Function;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+
+public class ConslistTest
+{
+    private static int NUM = 5;
+    private static Function<Integer, Boolean> PREDICATE = x -> x <= 2;
+    private static Function<Integer, Integer> FUNCTION = x -> x * 3;
+
+    private ConsList<Integer> EMPTY;
+    private ConsList<Integer> ONE_ELEM;
+
+    @Before
+    public void init()
+    {
+        EMPTY = ConsList.empty();
+        ONE_ELEM = ConsList.cons(NUM, ConsList.empty());
+    }
+
+    @Test
+    public void isEmptyOnEmptyList()
+    {
+        assertTrue(EMPTY.isEmpty());
+    }
+
+    @Test
+    public void isEmptyOnNonEmptyList()
+    {
+        assertFalse(ONE_ELEM.isEmpty());
+    }
+
+    @Test
+    public void lengthOnEmptyList()
+    {
+        assertEquals(0, EMPTY.length());
+    }
+
+    @Test
+    public void lengthOnNonEmptyList()
+    {
+        ConsList<Integer> myList = ConsList.cons(NUM, ConsList.cons(NUM, ConsList.empty()));
+
+        assertEquals(2, myList.length());
+    }
+
+    @Test
+    public void append()
+    {
+        ConsList<Integer> myList = EMPTY.append(NUM).append(NUM);
+
+
+        assertFalse(myList.isEmpty());
+        assertEquals(2, myList.length());
+        assertEquals(myList.head().longValue(), NUM);
+        assertEquals(myList.tail().head().longValue(), NUM);
+    }
+
+    @Test
+    public void reverse()
+    {
+        ConsList<Integer> myList = ConsList.cons(1, ConsList.cons(2, ConsList.cons(3, ConsList.empty())));
+        ConsList<Integer> myListReversed = ConsList.cons(3, ConsList.cons(2, ConsList.cons(1, ConsList.empty())));
+
+        ConsList<Integer> result = myListReversed.reverse();
+        assertEquals(myList, myListReversed.reverse());
+    }
+
+    @Test
+    public void reverseOnEmptyList()
+    {
+        assertEquals(EMPTY, EMPTY.reverse());
+    }
+
+    @Test
+    public void takeWhile()
+    {
+        ConsList<Integer> myList = ConsList.cons(1, ConsList.cons(2, ConsList.cons(3, ConsList.cons(1, ConsList.empty()))));
+        ConsList<Integer> actualList = myList.takeWhile(PREDICATE);
+        ConsList<Integer> expectedList = ConsList.cons(1, ConsList.cons(2, ConsList.empty()));
+
+        assertEquals(expectedList, actualList);
+    }
+
+    @Test
+    public void takeWhileOnEmptyList()
+    {
+        assertEquals(EMPTY, EMPTY.takeWhile(PREDICATE));
+    }
+
+    @Test
+    public void takeWhileOnFirstElem()
+    {
+        ConsList<Integer> myList = ConsList.cons(1, ConsList.cons(2, ConsList.cons(3, ConsList.cons(1, ConsList.empty()))));
+
+        assertEquals(myList.takeWhile(x -> x < 1), ConsList.empty());
+    }
+
+    @Test
+    public void takeWhileOnAllElements()
+    {
+        ConsList<Integer> myList = ConsList.cons(1, ConsList.cons(2, ConsList.cons(3, ConsList.cons(1, ConsList.empty()))));
+
+        assertEquals(myList.takeWhile(x -> x > 0), myList);
+    }
+
+    @Test
+    public void dropWhile()
+    {
+        ConsList<Integer> myList = ConsList.cons(1, ConsList.cons(2, ConsList.cons(3, ConsList.cons(1, ConsList.empty()))));
+        ConsList<Integer> actualList = myList.dropWhile(PREDICATE);
+        ConsList<Integer> expectedList = ConsList.cons(3, ConsList.cons(1, ConsList.empty()));
+
+        assertEquals(expectedList, actualList);
+    }
+
+    @Test
+    public void dropWhileOnEmptyList()
+    {
+        assertEquals(EMPTY, EMPTY.dropWhile(PREDICATE));
+    }
+
+    @Test
+    public void filter()
+    {
+        ConsList<Integer> myList = ConsList.cons(1, ConsList.cons(2, ConsList.cons(3, ConsList.cons(1, ConsList.empty()))));
+        ConsList<Integer> actualList = myList.filter(PREDICATE);
+        ConsList<Integer> expectedList = ConsList.cons(1, ConsList.cons(2, ConsList.cons(1, ConsList.empty())));
+
+        assertEquals(expectedList, actualList);
+    }
+
+    @Test
+    public void filterOnEmptyList()
+    {
+        assertEquals(EMPTY, EMPTY.filter(PREDICATE));
+    }
+
+    @Test
+    public void map()
+    {
+        ConsList<Integer> myList = ConsList.cons(1, ConsList.cons(2, ConsList.cons(3, ConsList.cons(1, ConsList.empty()))));
+        ConsList<Integer> actualList = myList.map(FUNCTION);
+        ConsList<Integer> expectedList = ConsList.cons(3, ConsList.cons(6, ConsList.cons(9, ConsList.cons(3, ConsList.empty()))));
+
+        assertEquals(expectedList, actualList);
+    }
+
+    @Test
+    public void mapOnEmptyList()
+    {
+        assertEquals(EMPTY, EMPTY.map(FUNCTION));
+    }
+}
